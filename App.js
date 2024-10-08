77e13b346b03a719cefbd0b070cbf29e15417f60
Carlos Fernandez <dominguezalacid@gmail.com>
2024-10-08 21:51:14 +0200

    actualizar eslint

--- App.js
+++ App.js
@@ -1,15 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, Pressable, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
 import { Image } from 'react-native-web';
 
-import icon from'./assets/icon.png';
+// import icon from'./assets/icon.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Image source={icon} style={{ width: 100, height: 100 }} />
+      <StatusBar style="light" /> 
+      {/* <Image source={icon} style={{ width: 100, height: 100, resizeMode: 'contain' }} /> */}
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* <Button title="Pulsa aqui" onPress={() => alert('Hola que tal')}/> */}
+      {/* boton personalizado */}
+      <Pressable
+      underlayColor={"#fff"} 
+      style={{width: 100, height: 100, backgroundColor: '#fff', borderRadius: 100,
+      alignItems: 'center', justifyContent: 'center'
+      }}
+      >{({pressed}) => (
+        <Text style={{color: pressed ? "#000" : "#fff"}}>Hola</Text>
+      )}
+      </Pressable>
     </View>
   );
 }
@@ -17,7 +28,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'whitesmoke',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
